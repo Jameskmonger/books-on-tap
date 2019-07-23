6e2ab7f3365b7b7b91b0488dac928fe22b3561ec
James Monger <jameskmonger@hotmail.co.uk>
2019-07-23 21:53:27 +0100

    Throw if response not ok

--- src/utils/fetch-json.ts
+++ src/utils/fetch-json.ts
@@ -2,6 +2,10 @@ export const fetchJson = async <T>(url: string) => {
     try {
         const response = await fetch(url);
 
+        if (!response.ok) {
+            throw new Error(`Error during fetch: ${response.status}`);
+        }
+
         const json = await response.json();
 
         return json as T;
